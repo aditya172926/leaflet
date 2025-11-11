4dc74925e4f451f36a97bb184994e6ffda4a033f
aditya172926 <aditya26sg@gmail.com>
2025-11-12 01:40:25 +0530

    fix: fix scrolling by redrawing immediately after event

--- stomata-cli/src/main.rs
+++ stomata-cli/src/main.rs
@@ -33,6 +33,8 @@ async fn main() -> anyhow::Result<()> {
             if let Event::Key(key) = event::read()? {
                 // handle events
                 app.handle_events(key)?;
+                // redraw immediately after an event
+                terminal.draw(|frame| app.render(frame))?;
             }
         }
 

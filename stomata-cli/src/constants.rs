88681c6c6232174aec0fc46c56b1699ad309e338
aditya172926 <aditya26sg@gmail.com>
2025-11-24 20:51:51 +0530

    feat: network display grid

--- stomata-cli/src/constants.rs
+++ stomata-cli/src/constants.rs
@@ -1,3 +1,3 @@
 pub const MAX_HISTORY: usize = 1000;
-pub const MAX_NETWORK_IN_MEMORY: usize = 1000;
+pub const MAX_NETWORK_IN_MEMORY: usize = 40;
 pub const MAX_HISTORY_IN_MEMORY: usize = 60;
--- stomata-cli/src/renders/displays/display_network.rs
+++ stomata-cli/src/renders/displays/display_network.rs
@@ -3,8 +3,9 @@ use std::collections::HashMap;
 use ratatui::{
     Frame,
     layout::{Constraint, Layout, Rect},
+    widgets::Sparkline,
 };
-use stomata_core::NetworkMetrics;
+use stomata_core::{NetworkMetrics, collectors::network::metrics::NetworkInterfaces};
 
 use crate::{
     renders::{
@@ -24,11 +25,7 @@ impl Display for NetworkMetrics {
         let number_of_interfaces: u16 = self.interfaces.len().try_into().unwrap_or(5);
         let constraints =
             vec![Constraint::Percentage((100 / number_of_interfaces)); number_of_interfaces.into()];
-        let layout1 = Layout::horizontal([
-            Constraint::Percentage(50),
-            Constraint::Percentage(50)
-        ]).split(area);
-        let layout = Layout::vertical(&constraints).split(layout1[0]);
+        let layout = Layout::horizontal(&constraints).split(area);
 
         if let Some(ui_state) = ui_state {
             let map = ui_state.networks_state.get_or_insert(HashMap::new());
@@ -40,22 +37,75 @@ impl Display for NetworkMetrics {
 
                 iface.update_network_history(interface);
 
-                let sparkline_title = format!(
-                    "Interface name: {} - Bytes received: {} - Bytes transmitted: {} - Total Bytes Received: {} - Total Bytes Transmitted: {} - Packets Received: {} - Packet transmitted: {}",
-                    interface.name,
-                    interface.bytes_received,
-                    interface.bytes_transmitted,
-                    interface.total_bytes_received,
-                    interface.total_bytes_transmitted,
-                    interface.packets_received,
-                    interface.packets_transmitted
+                let received_bytes_sparkline_title = format!(
+                    "Interface name: {} - Bytes received: {}",
+                    interface.name, interface.bytes_received
                 );
-                let sparkline_widget =
-                    render_sparkline(iface.received_bytes.make_contiguous(), &sparkline_title);
 
-                
-                // let network_para_widget = paragraph_widget(&network_para_info, &interface.name);
-                frame.render_widget(sparkline_widget, layout[index]);
+                let transmitted_bytes_sparkline_title = format!(
+                    "Interface name: {} - Bytes transmitted: {}",
+                    interface.name, interface.bytes_transmitted
+                );
+
+                let packets_received_sparkline_title = format!(
+                    "Interface name: {} - Packets received: {}",
+                    interface.name, interface.packets_received
+                );
+
+                let packets_transmitted_sparkline_title = format!(
+                    "Interface name: {} - Packets transmitted: {}",
+                    interface.name, interface.packets_transmitted
+                );
+
+                let errors_received_sparkline_title = format!(
+                    "Interface name: {} - Errors Received: {}",
+                    interface.name, interface.errors_on_received
+                );
+
+                let errors_transmitted_sparkline_title = format!(
+                    "Interface name: {} - Errors Transmitted: {}",
+                    interface.name, interface.errors_on_transmitted
+                );
+
+                //-- widgets --
+                let sparkline_widgets = vec![
+                    render_sparkline(
+                        iface.received_bytes.make_contiguous(),
+                        &received_bytes_sparkline_title,
+                    ),
+                    render_sparkline(
+                        iface.transmitted_bytes.make_contiguous(),
+                        &transmitted_bytes_sparkline_title,
+                    ),
+                    render_sparkline(
+                        iface.packets_received.make_contiguous(),
+                        &packets_received_sparkline_title,
+                    ),
+                    render_sparkline(
+                        iface.packets_transmitted.make_contiguous(),
+                        &packets_transmitted_sparkline_title,
+                    ),
+                    render_sparkline(
+                        iface.errors_received.make_contiguous(),
+                        &errors_received_sparkline_title,
+                    ),
+                    render_sparkline(
+                        iface.errors_transmitted.make_contiguous(),
+                        &errors_transmitted_sparkline_title,
+                    ),
+                ];
+
+                let secondart_constraints =
+                    vec![
+                        Constraint::Percentage(100 / sparkline_widgets.len() as u16);
+                        sparkline_widgets.len()
+                    ];
+                let secondary_layout =
+                    Layout::vertical(&secondart_constraints).split(layout[index]);
+
+                for (widget_index, widget) in sparkline_widgets.iter().enumerate() {
+                    frame.render_widget(widget, secondary_layout[widget_index]);
+                }
             }
         }
         Ok(())

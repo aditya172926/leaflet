56ea03c6421a0bcb518d74f955ae704f572a3991
aditya172926 <aditya26sg@gmail.com>
2025-11-24 21:33:23 +0530

    fix: fix the para layout lines in terminal

--- stomata-cli/src/renders/displays/display_network.rs
+++ stomata-cli/src/renders/displays/display_network.rs
@@ -22,10 +22,15 @@ impl Display for NetworkMetrics {
         area: Rect,
         ui_state: Option<&mut UIState>,
     ) -> anyhow::Result<()> {
+        let parent_layout =
+            Layout::vertical([Constraint::Length(8), Constraint::Min(1)]).split(area);
+
         let number_of_interfaces: u16 = self.interfaces.len().try_into().unwrap_or(5);
         let constraints =
             vec![Constraint::Percentage((100 / number_of_interfaces)); number_of_interfaces.into()];
-        let layout = Layout::horizontal(&constraints).split(area);
+
+        let para_layout = Layout::horizontal(&constraints).split(parent_layout[0]);
+        let sparkline_layout = Layout::horizontal(&constraints).split(parent_layout[1]);
 
         if let Some(ui_state) = ui_state {
             let map = ui_state.networks_state.get_or_insert(HashMap::new());
@@ -37,35 +42,31 @@ impl Display for NetworkMetrics {
 
                 iface.update_network_history(interface);
 
-                let received_bytes_sparkline_title = format!(
-                    "Interface name: {} - Bytes received: {}",
-                    interface.name, interface.bytes_received
+                // -- para widgets --
+                let interface_metadata_info = format!(
+                    "Total Bytes received: {}\nTotal Bytes Transmitted: {}\nTotal Packets Received: {}\nTotal Packets Transmitted: {}\nTotal Errors on receive: {}\nTotal Errors on transmit: {}",
+                    interface.total_bytes_received,
+                    interface.total_bytes_transmitted,
+                    interface.total_packets_received,
+                    interface.total_packets_transmitted,
+                    interface.total_errors_on_received,
+                    interface.total_errors_on_transmitted
                 );
+                let metadata_para_widget =
+                    paragraph_widget(&interface_metadata_info, &interface.name);
 
-                let transmitted_bytes_sparkline_title = format!(
-                    "Interface name: {} - Bytes transmitted: {}",
-                    interface.name, interface.bytes_transmitted
-                );
+                // -- sparkline widgets --
+                let received_bytes_sparkline_title =
+                    format!("Bytes received: {}", interface.bytes_received);
 
-                let packets_received_sparkline_title = format!(
-                    "Interface name: {} - Packets received: {}",
-                    interface.name, interface.packets_received
-                );
+                let transmitted_bytes_sparkline_title =
+                    format!("Bytes transmitted: {}", interface.bytes_transmitted);
 
-                let packets_transmitted_sparkline_title = format!(
-                    "Interface name: {} - Packets transmitted: {}",
-                    interface.name, interface.packets_transmitted
-                );
+                let packets_received_sparkline_title =
+                    format!("Packets received: {}", interface.packets_received);
 
-                let errors_received_sparkline_title = format!(
-                    "Interface name: {} - Errors Received: {}",
-                    interface.name, interface.errors_on_received
-                );
-
-                let errors_transmitted_sparkline_title = format!(
-                    "Interface name: {} - Errors Transmitted: {}",
-                    interface.name, interface.errors_on_transmitted
-                );
+                let packets_transmitted_sparkline_title =
+                    format!("Packets transmitted: {}", interface.packets_transmitted);
 
                 //-- widgets --
                 let sparkline_widgets = vec![
@@ -85,14 +86,6 @@ impl Display for NetworkMetrics {
                         iface.packets_transmitted.make_contiguous(),
                         &packets_transmitted_sparkline_title,
                     ),
-                    render_sparkline(
-                        iface.errors_received.make_contiguous(),
-                        &errors_received_sparkline_title,
-                    ),
-                    render_sparkline(
-                        iface.errors_transmitted.make_contiguous(),
-                        &errors_transmitted_sparkline_title,
-                    ),
                 ];
 
                 let secondart_constraints =
@@ -101,11 +94,12 @@ impl Display for NetworkMetrics {
                         sparkline_widgets.len()
                     ];
                 let secondary_layout =
-                    Layout::vertical(&secondart_constraints).split(layout[index]);
+                    Layout::vertical(&secondart_constraints).split(sparkline_layout[index]);
 
                 for (widget_index, widget) in sparkline_widgets.iter().enumerate() {
                     frame.render_widget(widget, secondary_layout[widget_index]);
                 }
+                frame.render_widget(metadata_para_widget, para_layout[index]);
             }
         }
         Ok(())

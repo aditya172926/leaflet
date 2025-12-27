862e175500f99e904b1165095ebf232464e5a829
aditya172926 <aditya26sg@gmail.com>
2025-12-28 02:39:57 +0530

    feat: decrypt screct fn

--- stomata-web3/src/providers/encrypt_secret.rs
+++ stomata-web3/src/providers/encrypt_secret.rs
@@ -50,3 +50,46 @@ pub fn encrpt_private_key(pk: &[u8], password: &str) -> Option<EncryptPrivateKey
         },
     })
 }
+
+pub fn decrypt_private_key(data: &EncryptPrivateKey, password: &str) {
+    let salt = match hex::decode(data.crypto_key.salt) {
+        Ok(salt) => salt,
+        Err(err) => {
+            eprintln!("Error in decoding salt {:?}", err);
+            return None;
+        }
+    };
+
+    let nonce = match hex::decode(data.crypto_key.nonce) {
+        Ok(nonce) => nonce,
+        Err(err) => {
+            eprintln!("Error in decoding nonce {:?}", err);
+            return None;
+        }
+    };
+
+    let ciphertext = match hex::decode(data.crypto_key.ciphertext) {
+        Ok(ciphertext) => ciphertext,
+        Err(err) => {
+            eprintln!("Error in decoding ciphertext {:?}", err);
+            return None;
+        }
+    };
+
+    let key = derive_key(password, &salt);
+    let cipher = match Aes256Gcm::new_from_slice(&key) {
+        Ok(cipher) => cipher,
+        Err(err) => {
+            eprintln!("Error in generating cipher {:?}", err);
+            return None;
+        }
+    };
+
+    match cipher.decrypt(Nonce::from_slice(&nonce), ciphertext.as_ref()) {
+        Ok(res) => Some(res),
+        Err(err) => {
+            eprintln!("Error in decrypting key {:?}", err);
+            return None;
+        }
+    }
+}

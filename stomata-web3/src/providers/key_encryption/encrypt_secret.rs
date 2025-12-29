5fdac739a3ca3896c10607bf86a39a63486acdb4
aditya172926 <aditya26sg@gmail.com>
2025-12-30 00:23:07 +0530

    refactor: dir for key encryption

--- stomata-web3/src/providers/key_encryption/encrypt_secret.rs
+++ stomata-web3/src/providers/key_encryption/encrypt_secret.rs
@@ -1,4 +1,4 @@
-use std::{io, path::PathBuf};
+use std::{fs, io, path::PathBuf};
 
 use aes_gcm::{Aes256Gcm, KeyInit, Nonce, aead::Aead};
 use argon2::Argon2;
@@ -151,7 +151,31 @@ pub fn get_storage_directory() -> Result<PathBuf, StorageError> {
     Ok(storage_dir)
 }
 
+/// Get the directory where encrypted keys are stored
+pub fn get_keys_dir() -> Result<PathBuf, StorageError> {
+    let storage_dir = get_storage_directory()?;
+    let keys_dir = storage_dir.join("keys");
+    Ok(keys_dir)
+}
 
+/// Create the storage dirs if they don't exist
+pub fn init_storage() -> Result<(), StorageError> {
+    let keys_dir = get_keys_dir()?;
+    
+    if !keys_dir.exists() {
+        fs::create_dir_all(&keys_dir)?;
+        
+        // Set restrictive permissions on Unix
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+            let permissions = fs::Permissions::from_mode(0o700);
+            fs::set_permissions(&keys_dir, permissions)?;
+        }
+    }
+    
+    Ok(())
+}
 
 #[cfg(test)]
 mod tests {
--- stomata-web3/src/providers/mod.rs
+++ stomata-web3/src/providers/mod.rs
@@ -1,2 +1,4 @@
 pub mod address;
-pub mod encrypt_secret;
+pub mod key_encryption;
+
+pub use key_encryption::encrypt_secret;
